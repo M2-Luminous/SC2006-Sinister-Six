1bfc15403b2737d835343d7df147f86130e4dea9
Minjuan Luo <93130757+M2-Luminous@users.noreply.github.com>
2022-09-19 23:41:08 +0800

    added main function to test
    still don't quite understand what is the meaining of featureVector mean, but the program is now available to run
    waiting for connection to the current database that we are using

--- Application/PredictPrice.java
+++ Application/PredictPrice.java
@@ -49,3 +49,24 @@ public class PredictPrice implements Function<Double[], Double> {
     }
 
 }
+
+/*
+package com.tonasolution.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       double[] thetaVector = new double[] { 1.004579, 5.286822 };
+
+	   LinearRegressionFunction targetFunction = new LinearRegressionFunction(thetaVector);
+       Double[] featureVector = new Double[] { 1.0, 1330.0 };
+
+       double predictedPrice = targetFunction.apply(featureVector);
+
+        System.out.println("The predicted price : " + predictedPrice);
+
+    }
+}
+
+*/
