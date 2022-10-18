fc529ead1beeb036d349a30666140efb20d9b573
KuroZenon <xxzenonokrxx@gmail.com>
2022-10-18 11:24:12 +0800

    backend Implementation of prediction

--- firebase/public/Filter.js
+++ firebase/public/Filter.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors'); 
+const bodyParser = require('body-parser');
 const formula = require('./formula')
 let conversion;
 let townData;
 let dateTime;
 const app = express();
+const router = express.Router();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', }));
+var allowCrossDomain = function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();  
+  }
+  app.use(allowCrossDomain);
 
+//Read conversion.json
 fs.readFile("./resources/data/conversion.json", "utf8", (err, jsonString) => {
     if (err) {
         console.log("File read failed:", err);
@@ -33,6 +46,7 @@ const MIN_FLOOR = 1;
 const MAX_FLOOR = 51;
 const averageFloor = [2, 3, 5, 8, 11, 13, 14, 17, 18, 20, 23, 26, 28, 29, 32, 33, 35, 38, 41, 44, 47, 50];
 
+//set Filter
 const SetFilter = (data) => {
     area = parseInt(data['floorArea']);
     floor = parseInt(data['floor']);
@@ -75,27 +89,30 @@ const test = () => {
     leaseStartDate = 1980;
 }
 
-
+//get Filter
 const Filter = () => {
     let filterObj = { area, floor, town, model, roomNo, leaseStartDate };
     return filterObj;
 }
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 
-app.post("/filter", (req, res) => {
-const t = req.query;
-const rawData = JSON.parse(req.query['data']);
+app.post("/filterReq", (req, res) => { 
+const rawData = req.body;
 SetFilter(rawData);
-console.log('Data collected');
-res.send({price: formula(Filter())});
-console.log('Data sent');
+console.log('Data collected ' + Filter()['area']);
+res.send({data: formula(Filter())});
+console.log('Data sent ' + JSON.stringify(formula(Filter())));
+res.end();
 });
 
-app.post("/test", (req, res) => {
+/*app.get("/test", (req, res) => {
     const rawData = req.body;
-    res.send('Connection successful');
-});
+    //res.send({price: '1'});
+    res.status(200).json({price: '1'});
+});*/
 
-app.listen(5000, () => {
-    console.log(`Server is running on port 5000.`);
+app.listen(5001, () => {
+    console.log(`Server is running on port 3001.`);
 });
